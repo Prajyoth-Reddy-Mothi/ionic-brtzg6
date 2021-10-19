83c9698898a755cdd15fa9bbf346c2ff8979be0d
Prajyoth-Reddy-Mothi <prajyothmothi2004@gmail.com>
2021-10-19 12:00:19 +0530

    Initial commit

---
+++ pages/auth/auth.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-contact',
+  templateUrl: 'auth.html',
+})
+export class AuthPage {
+  constructor(public navCtrl: NavController) {}
+}
